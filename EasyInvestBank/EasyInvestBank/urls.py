75aa1fa3fbc74ab1d1cf30dfa977d3eb4357267e
RistoSd <Risto.soodla@gmail.com>
2022-09-11 12:22:59 +0300

    urls format

--- EasyInvestBank/EasyInvestBank/urls.py
+++ EasyInvestBank/EasyInvestBank/urls.py
@@ -1,35 +1,16 @@
-"""EasyInvestBank URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path, re_path
-from Bank.views import  bank_view
+from django.urls import path
+from Bank.views import bank_view
 from User.views import registration_view, login_form, logout_view, AccountView
 from news.views import home
-from cryptoapi.views import cryptoprice
-
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # re_path(r"^money_transfer/", money_transfer, name = "money_transfer"),
     path('register/', registration_view, name='register'),
     path('login/', login_form, name='login'),
     path('', home, name='home'),
     path('logout/', logout_view, name='logout'),
     path('bank/', bank_view, name='bank'),
-    path('cryptoprice/', cryptoprice),
     path('account/', AccountView.as_view(), name='account')
 ]
