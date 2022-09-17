7184c9b17441f6ce628f95eefd18d6a8fc9f3d6f
Sandetto <Sand3tto@gmail.com>
2022-09-17 08:06:16 +0300

    Updated my account

--- EasyInvestBank/User/forms.py
+++ EasyInvestBank/User/forms.py
@@ -1,4 +1,3 @@
-from tkinter import Widget
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
 from django.contrib.auth import authenticate
@@ -33,37 +32,27 @@ class AccountAuthenticationForm(forms.ModelForm):
         email = self.cleaned_data['email']
         password = self.cleaned_data['password']
         if not authenticate(email=email, password=password):
-            raise forms.ValidationError('Wrong email or password')
+            raise forms.ValidationError('Wrong email or password!')
 
-class AccountPasswordChangeForm(PasswordChangeForm):
-    class Meta:
-        model = Account
-        fields = ('old_password', 'new_password1', 'new_password2')
-
-    widgets = {
-        'old_password': forms.PasswordInput(
-            attrs={'class': 'form-control'}
-        ),
-        'new_password1': forms.PasswordInput(
-            attrs={'class': 'form-control'}
-        ),
-        'new_password2': forms.PasswordInput(
-            attrs={'class': 'form-control'}
-        )
-    }
 
 class PersonalInformationForm(forms.ModelForm):
     
     class Meta:
         model = Account
-        fields = ['full_name', 'address']
-        exclude = ('date_of_birth', )
+        fields = ['full_name', 'address',]
+        exclude = ('date_of_birth', 'currency', 'country',)
         
         
-
 class EmailUpdateForm(forms.ModelForm):
+    email = forms.EmailField(max_length=255, widget=(forms.EmailInput(attrs={
+        'class': 'form-control',
+    })))
     
     class Meta:
         model = Account
         fields = ['email',]
-        
+   
+
+class PasswordChangeForm(PasswordChangeForm):
+    class Meta:
+        model = Account
